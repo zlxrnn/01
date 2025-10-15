7bc3c1548f4778da1cbefa1506fb57994d69ca58
Lxrnn <luxerne.mkv@gmail.com>
2025-10-15 16:24:24 +0800

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>SvelteKit App</title>
+</svelte:head>
+
+{@render children?.()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    let a = "skibidi";
+    let randompic = "https://preview.redd.it/yall-i-drew-the-travis-scott-fish-v0-xxlhlp5gbs591.jpg?width=640&crop=smart&auto=webp&s=8b6ec5245a685d602e9ae4e3b7479d90002845f4";
+    let o = 0;
+    
+    function skibidi()
+    {
+        o++;
+        if (o == 50) 
+        {
+            alert("Nenjah Bohemian");
+            o = 0; 
+        }
+    }
+
+</script>
+
+<h1>tailwind hilang</h1>
+<p>Lorem udin udin udin ipsum dolor sit amet consectetur adipisicing elit. 
+    Veritatis nostrum eos voluptatibus magnam alias maxime. Assumenda 
+    voluptatem nulla eum aliquid, maxime dolorem porro sunt facere reiciendis libero, 
+    architecto beatae saepe.</p>
+
+<img src={randompic} alt="">
+
+<button on:click={skibidi} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Click Me {o}
+</button>
+
+
