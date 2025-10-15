7e93dbe09034d3273be13c6f7bf0666c393a5794
Lxrnn <luxerne.mkv@gmail.com>
2025-10-15 17:40:30 +0800

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,52 @@
 <script>
+
+    import MyLovelyNavbar from "./navbar.svelte";
+
     let a = "skibidi";
     let randompic = "https://preview.redd.it/yall-i-drew-the-travis-scott-fish-v0-xxlhlp5gbs591.jpg?width=640&crop=smart&auto=webp&s=8b6ec5245a685d602e9ae4e3b7479d90002845f4";
     let o = 0;
+
+    
     
     function skibidi()
     {
         o++;
         if (o == 50) 
         {
-            alert("Nenjah Bohemian");
+            alert("Nenjah");
             o = 0; 
         }
     }
 
 </script>
 
-<h1>tailwind hilang</h1>
-<p>Lorem udin udin udin ipsum dolor sit amet consectetur adipisicing elit. 
-    Veritatis nostrum eos voluptatibus magnam alias maxime. Assumenda 
-    voluptatem nulla eum aliquid, maxime dolorem porro sunt facere reiciendis libero, 
-    architecto beatae saepe.</p>
+<style>
+    h1 {
+        align-items: center;
+        justify-content: center;
+        font-size: 30px;
+        font-weight: 700;
+        color: red;
+    }
+</style>
 
-<img src={randompic} alt="">
+<div class="container " >
+    <MyLovelyNavbar />
 
-<button on:click={skibidi} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-    Click Me {o}
-</button>
+    <h1>tailwind hilang</h1>
+    <p>Lorem udin udin udin ipsum dolor sit amet consectetur adipisicing elit. 
+        Veritatis nostrum eos voluptatibus magnam alias maxime. Assumenda 
+        voluptatem nulla eum aliquid, maxime dolorem porro sunt facere reiciendis libero, 
+        architecto beatae saepe.</p>
 
+    <img src={randompic} alt="">
 
+    <button on:click={skibidi} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Click Me {o}
+    </button>
+
+    <video controls autoplay loop muted>
+        <source src="/video/ommartin.mp4" type="video/mp4"/>
+        sayangggggggggggggggggggggggggggggggggggggggggggggggg
+    </video>
+</div>
---
+++ src/routes/navbar.svelte
@@ -0,0 +1,15 @@
+
+
+<nav class="navbar tercinta">
+    <ul>
+        <li>
+            <a href="google.com"> Skibiddi</a>
+        </li>
+        <li>
+            <a href="google.com"> Skibiddi</a>
+        </li>
+        <li>
+            <a href="google.com"> Skibiddi</a>
+        </li>
+    </ul>
+</nav>
